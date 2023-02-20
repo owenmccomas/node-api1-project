1ce6900a599fe106634826a6c3e6deacdfe8ca81
Owen McComas <owen@mccomas.me>
2023-02-20 16:33:32 -0700

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,3 +1,23 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// 'import' or 'require' express
+const express = require('express')
+
+
+// define server using express
+const server = express()
+
+// port defined
+const port = 8000
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+server.get('/', (req, res) => {
+
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
