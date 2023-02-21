26215d7885ff83570646adbc19f98ad65386b4ac
Owen McComas <owen@mccomas.me>
2023-02-20 17:43:10 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -3,9 +3,38 @@
 // 'import' or 'require' express
 const express = require('express')
 
+const User = require('./users/model')
 
 // define server using express
 const server = express()
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+        User.findById(req.params.id)
+        .then(user => {
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting user',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
 
 // port defined
 const port = 8000
