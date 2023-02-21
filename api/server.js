1162ab833e7e997fe9cf881ae746c075a55869d0
Owen McComas <owen@mccomas.me>
2023-02-20 18:49:29 -0700

    MVP completed

--- api/server.js
+++ api/server.js
@@ -15,21 +15,21 @@ server.put('/api/users/:id', async (req, res) => {
                 message: 'The user with the specified ID does not exist'
             })
         } else {
-            if (!req.body.name || !req.body.bio) {
+            if (!req.body.name || ! req.body.bio) {
                 res.status(400).json({
-                    message: 'Please provide name and bio for the user'
+                    message: 'Please provide a name and bio for the user'
                 })
             } else {
                 const updatedUser = await User.update(
                     req.params.id,
                     req.body,
-                 )
+                )
                 res.status(200).json(updatedUser)
             }
         }
     } catch (err) {
-        res.status(500).json({
-            message: 'error updating user',
+        res.status(400).json({
+            message: 'provide name and bio',
             err: err.message,
             stack: err.stack,
         })
