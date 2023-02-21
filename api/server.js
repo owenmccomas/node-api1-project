08dd381bf196639f87675a70decb3b8c73f94862
Owen McComas <owen@mccomas.me>
2023-02-20 18:04:57 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -7,6 +7,29 @@ const User = require('./users/model')
 
 // define server using express
 const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || ! user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user',
+        })
+    } else {
+        User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error creating user',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+})
+
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -15,7 +38,7 @@ server.get('/api/users', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({
-                message: 'error getting users',
+                message: 'error creating user',
                 err: err.message,
                 stack: err.stack,
             })
